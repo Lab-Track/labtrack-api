06aeada071f34381c3181cb072b95e1f7aa3bafd
Kênia <128853726+keniareis@users.noreply.github.com>
2026-09-10 18:27:30 -0300

    feat: adicionar queries de LoanItem e LoanReturn por equipamento (RF05)

---
+++ src/main/java/com/labtrack/labtrack/repository/LoanItemRepository.java
@@ -0,0 +1,20 @@
+package com.labtrack.labtrack.repository;
+
+import com.labtrack.labtrack.model.LoanItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface LoanItemRepository extends JpaRepository<LoanItem, Long> {
+
+    @Query("SELECT li FROM LoanItem li " +
+            "JOIN FETCH li.loan l " +
+            "JOIN FETCH l.student " +
+            "JOIN FETCH l.responsibleProfessor " +
+            "WHERE li.equipment.id = :equipmentId")
+    List<LoanItem> findByEquipmentId(@Param("equipmentId") Long equipmentId);
+}
